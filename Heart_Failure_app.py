f9dcfb93f357b0c0990c0aaf21c3fef8fb45d292
Whistlingwind <144652354+Whistlingwind@users.noreply.github.com>
2023-10-26 21:59:33 +0100

    Add files via upload

---
+++ Heart_Failure_app.py
@@ -0,0 +1,45 @@
+## Import streamlit to learn more visualisation tools, cause learning is cool.
+## This took ages to setup...!
+
+import streamlit as st
+
+from streamlit_jupyter import StreamlitPatcher, tqdm
+
+StreamlitPatcher().jupyter()  # register streamlit with jupyter-compatible wrappers
+
+## Import required libraries
+
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+       page_title="Heart Failure ANN",
+       page_icon="	:heartpulse:",
+       layout="wide"
+)
+
+st.title("Heart Failure Assignment")
+st.markdown("_Proto_")
+
+@st.cache_data
+def load_data(file):
+        data = pd.read_csv(file)
+
+        return data
+with st.sidebar:
+        st.header("Configuration")
+        uploaded_file = st.sidebar.file_uploader("Choose a file")
+#df = load_data("./heart_failure_clinical_records_dataset.csv")
+
+if uploaded_file is None:
+        st.info(" Upload a file through config", icon="ℹ️")
+        st.stop()
+
+df = load_data(uploaded_file)
+
+with  st.expander("Data Preview"):
+    st.markdown("_Proto_")
+    st.dataframe(df)
+
+with  st.expander("Data Preview2"):
+    st.dataframe(df)
