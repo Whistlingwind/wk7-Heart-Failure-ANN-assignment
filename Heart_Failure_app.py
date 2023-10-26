53bd3faebdec4aa37986469a17ea4a3d74e9e638
Whistlingwind <144652354+Whistlingwind@users.noreply.github.com>
2023-10-26 22:50:35 +0100

    Update Heart_Failure_app.py

--- Heart_Failure_app.py
+++ Heart_Failure_app.py
@@ -11,7 +11,7 @@ st.set_page_config(
        layout="wide"
 )
 
-st.title("Heart Failure Assignment7")
+st.title("Heart Failure Assignment8")
 st.markdown("_Proto_")
 
 
@@ -21,8 +21,7 @@ with st.sidebar:
         file = st.file_uploader("Upload a file", type="csv")
         if st.button('Get data'):
            df = pd.read_csv(file)
-           # This display will go away with the user's next action.
-           st.write(df)
+           
        
         if st.button('Save'):
            # This will always error.
